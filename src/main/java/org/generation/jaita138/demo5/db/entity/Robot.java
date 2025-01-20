e12f26856f53165cfb12230210b09cbf2348d1ba
Giovanni Cappelletti <tech@methodwolf.com>
2025-01-20 15:21:10 +0100

    consegna esercizio

---
+++ src/main/java/org/generation/jaita138/demo5/db/entity/Robot.java
@@ -0,0 +1,13 @@
+package org.generation.jaita138.demo5.db.entity;
+
+// TODO: definire entita' con
+// Entity + repo + service + test nel CommandLineRunner
+public class Robot {
+
+    // - id : Long : BigInt
+    // - nome : String : Varchar(64)
+    // - modello : String : Varchar(32)
+    // - altezza : int : Int
+    // - peso : int : Int
+    // - velocita : int : Int
+}
